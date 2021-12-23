458329299bfcf88566fd56892e35f7effe201807
zhouhaopkup <18190816904@163.com>
2021-12-23 18:30:29 +0800

    react-ts

---
+++ react-ts/src/app.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const App = ({ name }: { name: string }) => {
+  return <h1>{`Hello ${name}`}</h1>;
+};
+
+export default App;
---
+++ react-ts/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+};
