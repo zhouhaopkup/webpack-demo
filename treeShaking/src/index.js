61d5df21f1ae521bd224676beb669f63c240df05
zhouhaopkup <18190816904@163.com>
2022-01-04 17:16:13 +0800

    tree shaking

---
+++ treeShaking/src/index.js
@@ -0,0 +1,11 @@
+import { cube } from "./math";
+
+function component() {
+  const element = document.createElement("pre");
+  element.innerHTML = ["Hello webpack!", "5 cubed is equal to " + cube(5)].join(
+    "\n\n"
+  );
+  return element;
+}
+
+document.body.appendChild(component());
