77d66309816037cae244b29f28ef354c8323c0ca
zhouhaopkup <18190816904@163.com>
2022-01-04 14:08:51 +0800

    webpack-dev-server

---
+++ dev-server/src/index.js
@@ -0,0 +1,1 @@
+console.log("devServer works!");
---
+++ dev-server/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+    print: "./src/print.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devServer: {
+    static: "./dist",
+  },
+  devServer: {},
+};
