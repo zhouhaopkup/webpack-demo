4f89ef0d70e012b08b892e0b6a1a676696370eca
zhouhaopkup <18190816904@163.com>
2022-01-04 15:15:37 +0800

    hot module replacement

--- dev-server/webpack.config.js
+++ dev-server/webpack.config.js
@@ -1,16 +1,19 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
   mode: "development",
   entry: {
     index: "./src/index.js",
-    print: "./src/print.js",
+    hot: "webpack/hot/dev-server.js",
+    client: "webpack-dev-server/client/index.js?hot=true&live-reload=true",
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: "Development",
     }),
+    new webpack.HotModuleReplacementPlugin(),
   ],
   output: {
     filename: "[name].bundle.js",
@@ -19,6 +22,7 @@ module.exports = {
   },
   devServer: {
     static: "./dist",
+    hot: false,
+    client: false,
   },
-  devServer: {},
 };
